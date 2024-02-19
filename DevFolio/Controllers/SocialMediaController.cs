4275eee491ecac5e044330f13f2ef9cc0ad19932
Talha <99talhayildiz@gmail.com>
2024-02-20 02:44:02 +0300

    Completed the Skill Controller Crud Operations

---
+++ DevFolio/Controllers/SocialMediaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DevFolio.Models;
+
+namespace DevFolio.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        DbDevFolioEntities db = new DbDevFolioEntities();
+        public ActionResult SocialMediaList()
+        {
+            var values = db.TblSocailMedia.ToList();
+            return View(values);
+        }
+    }
+}
