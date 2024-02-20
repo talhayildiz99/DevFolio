855a80ae023eb739032177634d0c282c9abeae13
Talha <99talhayildiz@gmail.com>
2024-02-20 14:39:40 +0300

    Completed the Adress Controller Crud Operations

--- DevFolio/Controllers/AdressController.cs
+++ DevFolio/Controllers/AdressController.cs
@@ -10,10 +10,39 @@ namespace DevFolio.Controllers
     public class AdressController : Controller
     {
         DbDevFolioEntities db = new DbDevFolioEntities();
-        public ActionResult AddressList()
+        public ActionResult AdressList()
         {
             var values = db.TblAdress.ToList();
             return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateAdress()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateAdress(TblAdress p)
+        {
+            db.TblAdress.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("AdressList");
+        }
+
+        public ActionResult DeleteAdress(int id)
+        {
+            var value = db.TblAdress.Find(id);
+            db.TblAdress.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("AdressList");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateAdress(int id)
+        {
+            var value = db.TblAdress.Find(id);
+            return View(value);
         }
     }
 }
