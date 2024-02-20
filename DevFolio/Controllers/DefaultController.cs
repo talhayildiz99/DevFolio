c176521ea30c7df6bf691792383c8c1a5cc375ba
Talha <99talhayildiz@gmail.com>
2024-02-20 19:14:25 +0300

    Layout Uİ

---
+++ DevFolio/Controllers/DefaultController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DevFolio.Models;
+
+namespace DevFolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbDevFolioEntities1 db = new DbDevFolioEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFeature()
+        {
+            var values = db.TblFeature.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialProfile()
+        {
+            var values = db.TblProfile.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialSkill()
+        {
+            var values = db.TblSkill.ToList();
+            return PartialView(values);
+        }
+    }
+}
