f33bb09ec8167d6a5a81c92e3a5c73354468a8f3
Talha <99talhayildiz@gmail.com>
2024-02-20 15:05:43 +0300

    Completed the Service Controller Crud Operations

--- DevFolio/Controllers/ProfileController.cs
+++ DevFolio/Controllers/ProfileController.cs
@@ -3,15 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DevFolio.Models;
 
 namespace DevFolio.Controllers
 {
     public class ProfileController : Controller
     {
-        // GET: Profile
-        public ActionResult Index()
+        DbDevFolioEntities db = new DbDevFolioEntities();
+        public ActionResult ProfileList()
+        {
+            var values = db.TblProfile.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateProfile()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateProfile(TblProfile p)
+        {
+            db.TblProfile.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("ProfileList");
+        }
+        public ActionResult DeleteProfile(int id)
+        {
+            var value = db.TblProfile.Find(id);
+            db.TblProfile.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("ProfileList");
+
+        }
+
+        [HttpGet]
+        public ActionResult UpdateProfile(int id)
+        {
+            var value = db.TblProfile.Find(id);
+            return View(value);
         }
     }
 }
--- DevFolio/Controllers/ServiceController.cs
+++ DevFolio/Controllers/ServiceController.cs
@@ -3,14 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DevFolio.Models;
 
 namespace DevFolio.Controllers
 {
     public class ServiceController : Controller
     {
+        DbDevFolioEntities db = new DbDevFolioEntities();
         public ActionResult ServiceList()
         {
+            var values = db.TblService.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateService()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateService(TblService p)
+        {
+            db.TblService.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("ServiceList");
+        }
+
+        public ActionResult DeleteService(int id)
+        {
+            var value = db.TblService.Find(id);
+            db.TblService.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("ServiceList");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateService(int id)
+        {
+
+            var value = db.TblService.Find(id);
+            return View(value);
         }
     }
 }
