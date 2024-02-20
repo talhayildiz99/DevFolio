cebe6865fb44d9f2e07857d5b0f0f80611a579bb
Talha <99talhayildiz@gmail.com>
2024-02-20 17:05:48 +0300

    Completed the Statistic Controller

---
+++ DevFolio/Controllers/StatisticController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DevFolio.Models;
+
+namespace DevFolio.Controllers
+{
+    public class StatisticController : Controller
+    {
+        DbDevFolioEntities1 db = new DbDevFolioEntities1();
+        public ActionResult Index()
+        {
+            ViewBag.categoryCount = db.TblCategory.Count();
+            ViewBag.projectCount = db.TblProject.Count();
+            ViewBag.skillCount = db.TblSkill.Count();
+            ViewBag.skillAvgValue = db.TblSkill.Average(x => x.SkillValue);
+            ViewBag.lastSkillTitleName = db.GetLastSkillTitle().FirstOrDefault();
+            ViewBag.coreCategoryProjectCount = db.TblProject.Where(x => x.ProjectCategory == 1).Count();
+            return View();
+        }
+    }
+}
