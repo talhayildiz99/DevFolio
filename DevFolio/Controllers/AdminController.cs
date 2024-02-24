ff5cebbd11d2755cb78d84c5f0d5ac2037e7aedb
Talha <99talhayildiz@gmail.com>
2024-02-24 18:17:45 +0300

    Completed the Login Page

---
+++ DevFolio/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using DevFolio.Models;
+
+namespace DevFolio.Controllers
+{
+    public class AdminController : Controller
+    {
+        DbDevFolioEntities1 db = new DbDevFolioEntities1();
+ 
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(TblAdmin p)
+        {
+            var bilgi = db.TblAdmin.FirstOrDefault(x => x.Username == p.Username && x.Password == p.Password);
+            if (bilgi != null)
+            {
+                return RedirectToAction("Index", "AdminLayout");
+            }
+            else
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+        }
+    }
+}
