aecfbb46036dd7842e2cbe2cd95e0e88dc55ca00
Talha <99talhayildiz@gmail.com>
2024-02-24 16:39:36 +0300

    Completed the Dafeault Controller

--- DevFolio/Controllers/DefaultController.cs
+++ DevFolio/Controllers/DefaultController.cs
@@ -38,5 +38,62 @@ namespace DevFolio.Controllers
             var values = db.TblSkill.ToList();
             return PartialView(values);
         }
+        public PartialViewResult PartialAbout()
+        {
+            var values = db.TblAbout.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialService()
+        {
+            var values = db.TblService.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialStatistic()
+        {
+            ViewBag.skill = db.TblSkill.ToList().Where(i => Convert.ToInt32(i.SkillValue) >= 80).Count();
+            ViewBag.projects = db.TblProject.Count();
+            ViewBag.services = db.TblService.Count();
+            ViewBag.testimonial = db.TblTestimonial.Count();
+            return PartialView();
+        }
+        public PartialViewResult PartialPortfolio()
+        {
+            var project = db.TblProject.ToList();
+            return PartialView(project);
+        }
+        public PartialViewResult PartialTestimonial()
+        {
+            var testimonial = db.TblTestimonial.ToList();
+            return PartialView(testimonial);
+        }
+
+        [HttpGet]
+        public PartialViewResult PartialContact()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public JsonResult PartialContact(TblContact v)
+        {
+            db.TblContact.Add(v);
+            v.IsRead = false;
+            v.SendMessageDate = DateTime.Now;
+            db.SaveChanges();
+            ViewBag.m = "okey";
+            ViewBag.c = "show";
+            return Json("Mesajınız Gönderildi");
+        }
+
+        public PartialViewResult PartialAddress()
+        {
+            var address = db.TblAdress.ToList();
+            return PartialView(address);
+        }
+        public PartialViewResult PartialSocial()
+        {
+            var social = db.TblSocailMedia.Where(x => x.Status == true).ToList();
+            return PartialView(social);
+        }
     }
 }
