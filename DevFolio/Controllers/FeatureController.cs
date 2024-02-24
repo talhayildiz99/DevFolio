bf2740dca4d1dee3e1eadc67a433b321d94a1f59
Talha <99talhayildiz@gmail.com>
2024-02-24 17:18:11 +0300

    Completed the Feature Controller Crud Operations

--- DevFolio/Controllers/FeatureController.cs
+++ DevFolio/Controllers/FeatureController.cs
@@ -3,15 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DevFolio.Models;
 
 namespace DevFolio.Controllers
 {
     public class FeatureController : Controller
     {
-        // GET: Feature
-        public ActionResult Index()
+        DbDevFolioEntities1 db = new DbDevFolioEntities1();
+        public ActionResult FeatureList()
+        {
+            var values = db.TblFeature.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateFeature()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateFeature(TblFeature f)
+        {
+            db.TblFeature.Add(f);
+            db.SaveChanges();
+            return RedirectToAction("FeatureList");
+        }
+
+        public ActionResult DeleteFeature(int id)
+        {
+            var value = db.TblFeature.Find(id);
+            db.TblFeature.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("FeatureList");
+        }
+        [HttpGet]
+        public ActionResult UpdateFeature(int id)
+        {
+            var value = db.TblFeature.Find(id);
+            return View(value);
+        }
+
+
+        [HttpPost]
+        public ActionResult UpdateFeature(TblFeature p)
+        {
+            var value = db.TblFeature.Find(p.FeatureId);
+            value.HeaderTitle = p.HeaderTitle;
+            value.HeaderDescription = p.HeaderDescription;
+            db.SaveChanges();
+            return RedirectToAction("FeatureList");
         }
     }
 }
